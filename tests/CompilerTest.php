31e92e64a73422564a9b95fbcff4c879aa2f8aa0
Anton Vasiliev <1695147@gmail.com>
2023-10-22 21:38:29 +0100

    Add first test case

---
+++ tests/CompilerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phalcon\Volt\Test;
+
+use Phalcon\Volt\Compiler;
+use PHPUnit\Framework\TestCase;
+
+final class CompilerTest extends TestCase
+{
+    public function testCompileString(): void
+    {
+        $compiler = new Compiler();
+
+        $source   = '{{ str_replace("a", "b", "aabb") }}';
+        $expected = "<?= \$this->callMacro('str_replace', ['a', 'b', 'aabb']) ?>";
+        $actual   = $compiler->compileString($source);
+
+        $this->assertSame($expected, $actual);
+    }
+}
