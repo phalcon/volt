e11bb59e8a1fcdaefd70df039c4d30d35ce2cc94
Nikolaos Dimopoulos <nikos@niden.net>
2024-07-31 08:45:45 -0500

    added more tests

--- tests/unit/CallMacroTest.php
+++ tests/unit/CallMacroTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests;
+namespace Phalcon\Tests\Unit;
 
 use Phalcon\Volt\Compiler;
 use PHPUnit\Framework\TestCase;
---
+++ tests/unit/IsTagFactoryTest.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Phalcon\Tests\Unit;
+
+use Phalcon\Volt\Compiler;
+use PHPUnit\Framework\TestCase;
+use ReflectionObject;
+
+final class IsTagFactoryTest extends TestCase
+{
+    /**
+     * Tests Phalcon\Mvc\View\Engine\Volt :: isTagFactory()
+     *
+     * @dataProvider providerExamples
+     *
+     * @author       Phalcon Team <team@phalcon.io>
+     * @since        2024-07-31
+     */
+    public function testMvcViewEngineVoltIsTagFactory(
+        array $source,
+        bool $expected
+    ): void {
+        $parent = new Compiler();
+        $refected = new ReflectionObject($parent);
+        $method   = $refected->getMethod('isTagFactory');
+        $method->setAccessible( true );
+
+        $actual = $method->invoke($parent, $source);
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @return array[]
+     */
+    public static function providerExamples(): array
+    {
+        return [
+            [
+                [],
+                false,
+            ],
+            [
+                [
+                    'name' => [
+                        'left' => [
+                            'value' => 'tag',
+                        ],
+                    ],
+                ],
+                true,
+            ],
+            [
+                [
+                    'name' => [
+                        'left' => [
+                            'value' => 'something',
+                        ],
+                    ],
+                ],
+                false,
+            ],
+            [
+                [
+                    'name' => [
+                        'left' => [
+                            'name' => [
+                                'left' => [
+                                    'value' => 'tag',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                true,
+            ],
+            [
+                [
+                    'name' => [
+                        'left' => [
+                            'name' => [
+                                'left' => [
+                                    'value' => 'something',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                false,
+            ],
+        ];
+    }
+}
